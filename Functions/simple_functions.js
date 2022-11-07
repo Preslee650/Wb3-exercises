95ff119e8bc5c51cf21e1f06be4b1808b3df45a6
Michael Matinez <michaelm001@sfo.yearup.orggit config --global user.email michaelm001@sfo.yearup.orggit config --listgit config --global user.email michaelm001@sfo.yearup.org>
2022-11-07 15:15:15 -0800

    exercise 1 done
    creating and calling functions.

---
+++ Functions/simple_functions.js
@@ -0,0 +1,20 @@
+//func 1
+function favoriteThings() {
+let name='Michael';
+let movie='GoodFellas';
+let musician='BabyTron';
+console.log(name + movie + musician)
+}
+favoriteThings();
+
+//func 2
+function whyImHere() {
+console.log("To reach my highest potential")
+}
+whyImHere();
+
+//func 3
+function favoritePlace() {
+    console.log("Tokyo because I love anime")
+}
+favoritePlace();
