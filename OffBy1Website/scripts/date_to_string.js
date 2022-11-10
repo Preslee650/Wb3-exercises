bec8f0a940da077c906b77ace9c48487680ef7a8
Michael Matinez <michaelm001@sfo.yearup.orggit config --global user.email michaelm001@sfo.yearup.orggit config --listgit config --global user.email michaelm001@sfo.yearup.org>
2022-11-10 14:34:53 -0800

    date to strings practice

---
+++ OffBy1Website/scripts/date_to_string.js
@@ -0,0 +1,6 @@
+const currentDate=new Date();
+//each string function is like css for dates
+console.log(currentDate.toString())
+console.log(currentDate.toDateString())
+console.log(currentDate.toUTCString())
+console.log(currentDate.toLocaleString())
