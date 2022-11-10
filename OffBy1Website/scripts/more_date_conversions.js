b44de284286dd4f16e09a6c024edd91cfb02ca39
Michael Matinez <michaelm001@sfo.yearup.orggit config --global user.email michaelm001@sfo.yearup.orggit config --listgit config --global user.email michaelm001@sfo.yearup.org>
2022-11-10 14:54:15 -0800

    more date conversions exercise

---
+++ OffBy1Website/scripts/more_date_conversions.js
@@ -0,0 +1,9 @@
+//create date objects. display each using toLocaleString()
+const myBirthday=new Date("07/25/1999");// date variable being declared
+console.log(myBirthday.toLocaleString())//output using toLocaleString display function
+
+const momBirthday=new Date("001 18 1985");
+console.log(momBirthday.toLocaleString())
+
+const dadBirthday=new Date("1987, 2, 5");
+console.log(dadBirthday.toLocaleString())
